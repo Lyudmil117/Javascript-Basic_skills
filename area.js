d9f5b0b4685974608657a4f56aaa88e8e90299f5
Lyudmil Nakashev <110760786+Lyudmil117@users.noreply.github.com>
2022-11-07 18:13:18 +0200

    Add files via upload

--- area.js
+++ area.js
@@ -1,7 +1,25 @@
 function area(input){
-    let a = Number(input[0]);
-    let area = a * a;
-    console.log(area);
-}
+    let figure = input[0];
+    
+    if (figure === "square"){
+        let a = Number(input[1]);
+        let result = a * a;
+        console.log(result)
+    } else if (figure === "rectangle"){
+        let a = Number(input[1]);
+        let b = Number(input[2]);
+        let result = a * b;
+        console.log(result)
+    } else if (figure === "circle") {
+        let a = Number(input[1]);
+        let result = Math.PI * a * a;
+        console.log(result)
+    } else if (figure === "triangle") {
+        let a = Number(input[1]);
+        let b = Number(input[2]);
+        let result = (a * b) / 2;
+        console.log(result)
+    }
+}   
 
-area([4])
+area(["square", "4"])
