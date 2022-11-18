6e048b42b57d89211399a065969704d42645303c
Lyudmil Nakashev <110760786+Lyudmil117@users.noreply.github.com>
2022-11-18 19:10:39 +0200

    Add files via upload

---
+++ numberInRange.js
@@ -0,0 +1,10 @@
+function number(input){
+    let number = Number(input[0]);
+
+    if (-100 <= number && number <= 100 && number != 0){
+    console.log("Yes")
+    }  else { 
+    console.log("No");
+    }
+}
+number([101])
